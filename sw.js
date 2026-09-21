03687207b3ff79c33056ace8c34b305cbcd68d36
HOUZY HOME <BoppityBookml@planetmail.com>
2026-09-21 13:57:25 +0800

    Write reconciled HitPay status from portal

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // HOUZY HOME Service Worker
 // 版本号由 .githooks/pre-commit 自动注入(git 短 hash + 时间戳)
 // 改了此文件后请确保 hook 已启用:bash scripts/setup-pwa.sh
-const CACHE_VERSION = '7506e92-20260921043949';
+const CACHE_VERSION = '7dcda2a-20260921055725';
 const CACHE_NAME = 'houzy-' + CACHE_VERSION;
 
 // 安装时预缓存的基础资源(可选,首次安装就缓存)
